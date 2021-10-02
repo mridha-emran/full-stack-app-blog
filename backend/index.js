3dda3b1919aaf470533ec7bf0ebd75ef1b3fbb73
mridha-emran <bari.lala490@gmail.com>
2021-10-03 01:17:47 +0200

    back post done

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-const dotenv=require("dotenv");
-
+const dotenv=require("dotenv")
 const mongoose = require("mongoose");
 const authRouter=require("./routes/auth");
 const userRouter=require("./routes/user")
