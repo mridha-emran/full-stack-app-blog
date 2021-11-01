331a06823a566fe8725548a320b2c3a69c9d37f9
mridha-emran <bari.lala490@gmail.com>
2021-11-01 16:11:14 +0100

    catagories add

--- backend/index.js
+++ backend/index.js
@@ -5,21 +5,45 @@ const mongoose = require("mongoose");
 const authRouter=require("./routes/auth");
 const userRouter=require("./routes/user")
 const postRouter=require("./routes/post")
+const catagoriesRouter=require("./routes/catagories")
+const multer=require("multer");
+const cors = require("cors")
+const path = require("path");
 
 dotenv.config({
-    path:"./config.env"
+    
 })
 
-app.use(express.json())
+
 
 
 mongoose.connect(process.env.DB, {useNewUrlParser: true,})
-	.then(() => {
-		console.log("Connected to MongoDB !");
-	});
+.then(() => {
+	console.log("Connected to MongoDB !");
+});
 
+app.use(cors())
+app.use(express.json())
+
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "images");
+	},
+	filename: (req, file, cb) => {
+		cb(null,"file.jpg");
+	},
+});
+
+
+	  const upload = multer({ storage: storage });
+	  app.post("/upload", upload.single("file"), (req, res) => {
+		res.status(200).json("File has been uploaded");
+	  });
 	app.use("/auth",authRouter)
 	app.use("/user",userRouter)
 	app.use("/post",postRouter)
+	app.use("/catagories",catagoriesRouter)
 
     app.listen( process.env.PORT,()=>( console.log("this server listing port:8000")))
